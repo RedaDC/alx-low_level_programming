9e3ecdffba1093bb9c09179974ed23bd7717bf0c
RedaDC <redaabousaid234@gmail.com>
2023-08-29 16:18:54 +0100

    Saida

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,6 +10,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 		int j = 0, i = 0;
 		unsigned int a = 0;
+
 		while (s[i] != ' ' && s[i] != '\0')
 		{
 			while (accept[j] != '\0')
@@ -20,6 +21,6 @@ unsigned int _strspn(char *s, char *accept)
 			}
 				i++;
 				j = 0
-			}
+		}
 		return (a);
 }
