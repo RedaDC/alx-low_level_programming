c54c3b2e7c7eac64c57df5d5110ef90dae742800
RedaDC <redaabousaid234@gmail.com>
2023-08-30 20:50:26 +0100

    Task9

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,11 +9,11 @@
  */
 int _pow_recursion(int x, int y)
 {
-		if (y == 0)
-			return (1);
-		if (y < 0)
-			return (-1);
-		if (y > 0)
-			--y;
-			return (_pow_recursion(x, y) * x);
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
+	if (y > 0)
+		--y;
+	return (_pow_recursion(x, y) * x);
 }
